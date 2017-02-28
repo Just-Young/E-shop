34179fe91841d1ca47292deed4c0e739947f1c93
Administrator <2356740397@qq.com>
2017-02-28 09:07:03 +0800

    框架

--- src/main/java/com/ibeifeng/jp/service/UserService.java
+++ src/main/java/com/ibeifeng/jp/service/UserService.java
@@ -5,13 +5,14 @@ import com.ibeifeng.jp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Created by zmzy on 2017/2/23.
  */
 @Service
 public class UserService implements IUserService {
     @Autowired
-
     private IUserDao userDao;
 
     public void add(User user) {
@@ -22,13 +23,10 @@ public class UserService implements IUserService {
         return userDao.load(id);
     }
 
-//    @Override
-//    public void add(User user) {
-//        userDao.add(user);
-//    }
-//
-//    @Override
-//    public User load(int id) {
-//        return userDao.load(id);
-//    }
+    @Override
+    public List<User> list() {
+        return userDao.list();
+    }
+
+
 }
